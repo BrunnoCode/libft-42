bc6c6a21c5868a0fbe34c0aac9e92497fa414fac
Brunno Warley Botelho Rocha <bbotelho@cbr12s5.42barcelona.com>
2023-09-13 14:11:56 +0200

    loading

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 12:10:16 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/13 13:59:24 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/13 14:11:42 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 size_t	ft_strlcpy(char *dest, const *src, size_t destsize)
 {
+	size_t	i;
 
+	i = 0;
+	while (i < (destsize - 1) && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 }
+	size_t	len;
+
+	len = 0;
+	while ()
