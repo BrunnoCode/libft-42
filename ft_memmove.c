bfe1947d19e66224914c212672771b994a134c76
Brunno Warley Botelho Rocha <bbotelho@car3s3.42barcelona.com>
2023-09-18 12:13:16 +0200

    done

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 18:57:28 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/17 23:43:35 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/18 11:46:18 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,25 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	// Declaramos nossas variáveis
-	size_t i; // size_t i pois vamos comparar um size de len
-	char *d;  // Declaro um ponteiro que irá receber o meu dst
-	char *s;  // Faço o mesmo aqui pra receber o src
+	size_t	i;
+	char	*d;
+	char	*s;
 
-	d = (char *)d;
-		// Fazemos um cast para que nossa variável receba nossos ponteiros *dst *src
-	s = (char *)s;
+	d = (char *)dst;
+	s = (char *)src;
 	i = 0;
-	if (d > s) // se o destino for maior que origem
+	if (dst == 0 && src == 0)
+		return (NULL);
+	if (d > s)
 	{
-		while (0 < len) // exemplo: o total de len é 8,
-			que representa uma posicao do array de src
+		while (len-- > 0)
 		{
-			len--;          
-				// Entao em cada loop essa posicao vai diminuindo 8,7,6,5,4,3,2,1
 			d[len] = s[len];
-				// d vai recebendo o cada valor de s na posicao atual de len
 		}
 	}
-	else // se o destino for menor que origem os dados se sobrepoem normalmente
+	else
 	{
-		while (i < len - 1)
+		while (i < len)
 		{
 			d[i] = s[i];
 			i++;
