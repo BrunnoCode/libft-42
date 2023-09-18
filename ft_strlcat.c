29d82c1d9e3e31bc8ed396e4edbb44603b779219
Brunno Warley Botelho Rocha <bbotelho@car3s4.42barcelona.com>
2023-09-18 19:52:14 +0200

    continue

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:06:42 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/18 14:14:13 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:26:40 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,37 @@
 
 size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
 {
-	char *d;
-	char *s;
-	int size;
-	size_t i;
+	char	*d;
+	char	*s;
+	int		size;
+	size_t	i;
 
 	d = (char *)dst;
 	s = (char *)src;
 	i = 0;
-	size = ft_strlen(d);
-
-	if (dst == 0 && src == 0)
-		return (NULL);
-	while (size > s)
+	if (ft_strlen(dst) != dstsize)
+		return ('\0');
+	else
 	{
-        d
+		while (s != 0)
+		{
+			d[dstsize] = s[i];
+			i++;
+		}
 	}
+	return (dst + src);
+}
+#include <string.h>
+int	main(int c, char**v)
+{
+	if(c == 4)
+	{
+		printf("Tu resultado es: '%c'.\n ", ft_strlcat(v[1], v[2], atoi(v[3])));
+		printf("El resultado Original es: '%lu'.\n ", strlcat(v[1], v[2], atoi(v[3])));
+	}
+	else
+	{
+		printf("Error de argumentos :D.\n");
+	}
+	return (0);
 }
