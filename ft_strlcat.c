677543090df3d72475c26494f5b2b53f0633c4e6
Brunno Warley Botelho Rocha <bbotelho@cbr13s6.42barcelona.com>
2023-09-19 21:29:05 +0200

    correcao guillem

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 13:06:42 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/19 20:53:28 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/19 21:28:28 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	char	*s;
 	size_t	d_len;
 	size_t	s_len;
-	size_t	total_len;
 	size_t	i;
 
 	s = (char *)src;
 	d_len = ft_strlen(dst);
 	s_len = ft_strlen(s);
-	total_len = 0;
 	i = 0;
 	if (dstsize > d_len)
-		total_len = s_len + d_len;
+		s_len += d_len;
 	else
-		total_len = s_len + dstsize;
+		s_len += dstsize;
 	while (src[i] && (d_len + 1) < dstsize)
 	{
 		dst[d_len] = s[i];
@@ -36,28 +34,28 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		i++;
 	}
 	dst[d_len] = '\0';
-	return (total_len);
+	return (s_len);
 }
 /*
 #include <stdlib.h>
 #include <string.h>
 
-int	main(int argc, char **argv)
+int	main()
 {
-	char		*dst;
-	const char	*src;
+	char		dst[100] = "mi destino";
+	const char	*src = " espero que esto apare haya ejecutado la funcion";
 	size_t		destsize;
 	size_t		res;
 
-	if (argc != 4)
-		printf("error de argumento");
-	else
-		dst = argv[1];
-	src = argv[2];
-	destsize = (size_t)atoi(argv[3]);
+	destsize = 35;
 	printf("Destino antes de la funcion: \"%s\"\n", dst);
 	res = ft_strlcat(dst, src, destsize);
 	printf("Destino despues de la funcion: \"%s\"\n", dst);
-	printf("Destino antes de la funcion: \"%zu\"\n", res);
+	printf("Total despues de la funcion: \"%zu\"\n", res);
+	
+	printf("Destino antes de la funcion: \"%s\"\n", dst);
+	res = strlcat(dst, src, destsize);
+	printf("Destino despues de la funcion: \"%s\"\n", dst);
+	printf("Total despues de la funcion: \"%zu\"\n", res);
 	return (0);
 }*/
