01bcc2beeb3960d7c2ff62c99cfc2fcaa69feac1
Brunno Warley Botelho Rocha <bbotelho@cbr12s6.42barcelona.com>
2023-09-20 20:45:22 +0200

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:31:35 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/20 19:25:13 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/20 20:42:46 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,23 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != 0 && *s != c)
+	while (*s != (char*)c && *s != 0)
 	{
 		s++;
 	}
 	if (*s == c)
+	{
 		return ((char *)s);
-	return ((char *)'\0');
+	}
+	return ((char *) '\0');
 }
-/*
+
 #include <string.h>
-int main()
+/*
+int	main(void)
 {
-	char	s[] = "k573@#@%6$%g5";
-	int	c = '%';
+	char	s[] = "teste";
+	int	c = 357;
 	printf("En tu funcion Hemos encontrado: '%s' \n", ft_strchr(s, c));
 	printf("Funcion original: '%s' \n", strchr(s, c));
 	return (0);
