326a325390557ae0024f64bb84a675ad9148c432
Brunno Warley Botelho Rocha <bbotelho@car1s2.42barcelona.com>
2023-09-26 17:11:55 +0200

    done memmcp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 13:42:22 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/26 14:10:43 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/26 17:11:27 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,31 @@
 
 int	ft_memcmp(const void *p1, const void *p2, size_t n)
 {
-	unsigned char	*pu;
-	unsigned char	*pd;
+	unsigned const char	*pu;
+	unsigned const char	*pd;
+	size_t				i;
 
-	pu = (unsigned char *)p1;
-	pd = (unsigned char *)p2;
-	while (n-- && pu == pd)
+	pu = p1;
+	pd = p2;
+	i = 0;
+	while (i < n)
 	{
+		if (*pu != *pd)
+			return (*pu - *pd);
 		pu++;
 		pd++;
+		i++;
 	}
-	if (pu == pd && n == 0)
-		return (0);
-	return (pu - pd);
+	return (0);
 }
-
+/*
 #include <string.h>
 
 int	main(void)
 {
-	printf("Mi funcion: %d \n", ft_memcmp("hola que tal", "hola que tal", 20));
-	printf("funcion original: %d \n", memcmp("hoha que tal", "hola que tal",
-			20));
+	char *str1 = "teste";
+	char *str2 = "test";
+	printf("Mi funcion: %d \n", ft_memcmp(str1, str2, 10));
+	printf("funcion original: %d \n", memcmp(str1, str2, 10));
 	return (0);
-}
+}*/
