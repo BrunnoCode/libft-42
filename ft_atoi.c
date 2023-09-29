dc4d28bf7cfa1f7ad6685903a43d190290f6e18e
Brunno Warley Botelho Rocha <bbotelho@cbr10s4.42barcelona.com>
2023-09-29 22:01:02 +0200

    atoi done

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 18:33:51 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/29 19:17:04 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/29 21:59:55 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,8 @@ static int	ignore(const char *str)
 	int	i;
 
 	i = 0;
-	if (str[i] > 32 && str[i] < 46)
-		if ((str[i + 1] >= 32 && str[i + 1] <= 46) || str[i + 1] == '\n')
-			return (0);
 	while ((str[i] == ' ') || (str[i] == '\n') || (str[i] == '\t')
-		|| (str[i] == '\f') || (str[i] == '\v') || (str[i] == '\r')
-		|| (str[i] == '+'))
+		|| (str[i] == '\f') || (str[i] == '\v') || (str[i] == '\r'))
 		i++;
 	return (i);
 }
@@ -32,10 +28,9 @@ static int	isnegative(const char *str, int i)
 	int	signal;
 
 	signal = 1;
-	if (str[i] == '-' && str[i + 1] == '-')
-		return (0);
-	if (str[i] == '-')
-		signal = -1;
+	if (str[i] == '-' || str[i] == '+')
+		if (str[i] == '-')
+			signal = -1;
 	return (signal);
 }
 
@@ -50,14 +45,15 @@ int	ft_atoi(const char *str)
 	signal = isnegative(str, i);
 	if (!*str)
 		return (0);
-	while ((str[i] >= 48 && str[i] <= 57) || (str[i] == '-'))
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] >= 48 && str[i] <= 57)
 	{
 		if (str[i] >= 48 && str[i] <= 57)
 			num = num * 10 + (str[i] - '0');
 		i++;
 	}
 	return ((int)signal * num);
-	return (0);
 }
 /*
 #include <stdlib.h>
@@ -66,7 +62,7 @@ int	main(void)
 {
 	const char	*str;
 
-	str = "			+435";
+	str = "-43-5";
 	printf("Resultado mi funcion: %d\n", ft_atoi(str));
 	printf("funcion original: %d\n", atoi(str));
 	return (0);
