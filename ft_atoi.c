9f736f9f5a116139fbfcfa2fd5e25ceb376928cb
Brunno Warley Botelho Rocha <bbotelho@cbr11s2.42barcelona.com>
2023-09-29 12:12:02 +0200

    save

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 18:33:51 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/09/28 19:13:26 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/09/29 12:11:34 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,30 @@
 
 int	ft_atoi(const char *str)
 {
+		int	i;
+		int	signal;
+		int	num;
+
+		i = 0;
+		num = 0;
+		signal = -1;
+
+		if(!*str)
+			return (0);
+		while ((str[i] >= '0' && str[i] <= '9') || (str[i] == '-'))
+		{
+			if(str[0] == '-')
+				num = signal;
+			else if ()
+		}
+
+}
+#include <stdlib.h>
+int	main()
+{
+	const char	*str = "12 34";
+	printf("Resultado mi funcion: %d\n", ft_atoi(str));
+	printf("funcion original: %d\n", atoi(str));
+	return (0);
 	
 }
