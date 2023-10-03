e4697f974dd98e0263ec0c89ddd2c1f404a50829
Brunno Warley Botelho Rocha <bbotelho@cbr9s6.42barcelona.com>
2023-10-03 11:45:24 +0200

    strjoin done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:36:42 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/02 21:13:41 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/03 11:41:09 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int		len;
 	int		len1;
 	int		len2;
 	int		i;
@@ -25,25 +24,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	len = (len1 + len2);
-	if (!(str = (char *)malloc((len + 1) * sizeof(char))))
+	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
-	i = 0;
-	while (i < len--)
+	i = -1;
+	while (++i < len1)
 	{
 		str[i] = s1[i];
-		i++;
-		j = 0;
-		while (s2[j] < i && str[i] == ft_strlen(s1))
-		{
-			str[i] = s2[j];
-			j++;
-			i++;
-		}
 	}
-	str[j] = '\0';
+	j = -1;
+	while (++j < len2)
+	{
+		str[j + i] = s2[j];
+	}
+	str[j + i] = '\0';
 	return (str);
 }
+/*
 int	main(void)
 {
 	char	*s1;
@@ -51,6 +48,7 @@ int	main(void)
 
 	s1 = "hello";
 	s2 = "World";
-	printf("Resultado: %s \n", ft_strjoin(s1, s2));
+	printf("Resultado: '%s'\n memoria s1: '%p' \n memoria s2 '%p' \n",
+		ft_strjoin(s1, s2), s1, s2);
 	return (0);
-}
+}*/
