44923ccb0a7f4eaa373f2c6e7853a054a8defc0f
Brunno Warley Botelho Rocha <bbotelho@cbr9s6.42barcelona.com>
2023-10-03 14:43:42 +0200

    starting strtrim

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:59:15 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/02 20:36:24 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/03 13:13:38 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strjoin(const char *s1, const char *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
@@ -44,5 +43,7 @@ void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
 // FUNCIONES DE LA PARTE 2 (FUNCIONES ADICIONALES)
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
