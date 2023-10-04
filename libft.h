6517ce4acbb6c7acc6d32cab275a3e4515d1ea92
Brunno Warley Botelho Rocha <bbotelho@cbr11s2.42barcelona.com>
2023-10-04 21:16:19 +0200

    putchar

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bbotelho <bbotelho@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:59:15 by bbotelho          #+#    #+#             */
-/*   Updated: 2023/10/04 21:03:26 by bbotelho         ###   ########.fr       */
+/*   Updated: 2023/10/04 21:07:14 by bbotelho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalnum(int c);
 int		ft_toupper(int c);
